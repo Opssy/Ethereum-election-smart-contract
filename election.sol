ec77c21e1b28e066d0562e5daf21fd148ba08bf8
Opeyemi is <opeyemib39@gmail.com>
2018-05-16 12:40:25 +0100

    first commit

---
+++ election.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+contract Election {
+
+	struct candidates {
+		string names;
+		uint voteCounts;
+		
+	}
+	struct voters {
+		bool voted;
+		uint voteIndex;
+		
+	}
+	
+	
+}
