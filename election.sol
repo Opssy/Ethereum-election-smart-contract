68246715397e3588187330e41f627bed38fb36d1
Opeyemi is <opeyemib39@gmail.com>
2018-05-17 14:04:25 +0100

    merged

--- election.sol
+++ election.sol
@@ -13,5 +13,22 @@ contract Election {
 		
 	}
 	
-	
+	address public owner;
+	mapping (address => voters) public voters;
+
+	candidates[] public candidates;
+	unint public auctioneEnd;
+
+	function Election( string name, uint durationMinutes, string candidate1, string candidate2 ){
+		owner = msg.sender;
+		name = _name;
+		auctioneEnd = now + (durationMinutes*1 minutes);
+
+
+		candidates.push(candidates(candidate1, 0));
+		candidates.push(candidates(candidate2, 0));
+	} 
+	function authorize(address voter){
+		require()
+	}
 }
